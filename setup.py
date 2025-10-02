a4bec0ddc0da25766122a70a1e3bfa138221e29c
srvmann <sauravgrover567@gmail.com>
2025-10-02 12:04:07 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A ml project to detect fake news.',
+    author='Saurav',
+    license='MIT',
+)
